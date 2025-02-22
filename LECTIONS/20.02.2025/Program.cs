f7b567b9ba97d6049eb3591bba212c1edbba6b8e
Yanbellq <yanbellq@gmail.com>
2025-02-22 22:49:31 +0200

    Project: Lab1: task1 done, wip

---
+++ LECTIONS/20.02.2025/Program.cs
@@ -0,0 +1,31 @@
+﻿// Console.WriteLine("Hello, World!");
+
+string? x = null;
+
+string a = "a";
+string b = "b";
+
+var compareTo = a.CompareTo(b);
+var str = String.Concat(a, b);
+var contains = a.Contains(b);
+var joined = String.Join("qwe", new string[] { "a", "b", "c", "d" });
+// string? joined = "";
+// String.CompareOrdinal();
+// string? va = null;
+// char[] destination = new char[100];
+// joined.CopyTo(new char[] {});
+// joined.CopyTo(destination);
+// Console.WriteLine(destination);
+
+var startWith = joined.IndexOf("qwe");
+var insert = joined.Insert(3, "Maksym");
+Console.WriteLine(insert);
+
+var replace = insert.Replace("Rostyk", "");
+Console.WriteLine(replace);
+
+var strings = replace.Split(a, b);
+
+
+var substring = "hello world".Substring(3, 6);
+Console.WriteLine(substring.ToUpper());
--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static string CheckPoint(double x, double y, double R)
+    {
+        // Межі квадрату: він знаходиться у другому і четвертому квадрантах координатної площини
+        bool insideSquare = (x >= -R && x <= 0) && (y >= 0 && y <= R);
+
+        // Межі кіл: перевіряємо, чи точка всередині будь-якого з двох кіл
+        bool insideTopCircle = (Math.Pow(x, 2) + Math.Pow(y - R, 2) <= R * R);
+        bool insideBottomCircle = (Math.Pow(x, 2) + Math.Pow(y + R, 2) <= R * R);
+
+        // Перевірка потрапляння точки
+        if (insideSquare)
+            return "Так"; // Точка всередині заштрихованої області
+        if (insideTopCircle || insideBottomCircle)
+            return "Ні"; // Точка поза заштрихованою областю
+        return "На межі"; // Точка на межі фігури
+    }
+
+    static void Main()
+    {
+        Console.Write("Введіть координату x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть координату y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть радіус R: ");
+        double R = Convert.ToDouble(Console.ReadLine());
+
+        string result = CheckPoint(x, y, R);
+        Console.WriteLine($"Результат: {result}");
+    }
+}
