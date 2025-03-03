5e707f1f39e9c34a82bf20b02cf8174eaa494992
Yanbellq <yanbellq@gmail.com>
2025-03-03 16:36:18 +0200

    Project: Lab2 - task4 done, project finished

---
+++ lab2/task4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+
+        // Ініціалізація зубчастого масиву
+        int[][] jaggedArray = new int[5][];
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            jaggedArray[i] = new int[rand.Next(1, 6)];
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+            {
+                jaggedArray[i][j] = rand.Next(-10, 11);
+            }
+        };
+
+        // Визначення максимальної довжини рядка
+        int maxCols = 0;
+        foreach (var row in jaggedArray)
+        {
+            if (row.Length > maxCols)
+            {
+                maxCols = row.Length;
+            }
+        }
+
+        // Виведення стовпців
+        Console.WriteLine("Елементи кожного стовпця:");
+        for (int col = 0; col < maxCols; col++)
+        {
+            Console.Write($"Стовпець {col + 1}: ");
+            foreach (var row in jaggedArray)
+            {
+                if (col < row.Length)
+                {
+                    Console.Write(row[col] + "\t");
+                }
+                else
+                {
+                    Console.Write("\t");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        // Ініціалізація масиву для зберігання сум від'ємних елементів
+        int[] negativeSums = new int[maxCols];
+
+        // Підрахунок сум від'ємних елементів для кожного стовпця
+        for (int col = 0; col < maxCols; col++)
+        {
+            int sum = 0;
+            foreach (var row in jaggedArray)
+            {
+                if (col < row.Length && row[col] < 0)
+                {
+                    sum += row[col];
+                }
+            }
+            negativeSums[col] = sum;
+        }
+
+        // Виведення результатів
+        Console.WriteLine("\nСуми від'ємних елементів для кожного стовпця:");
+        for (int i = 0; i < negativeSums.Length; i++)
+        {
+            Console.WriteLine($"Стовпець {i + 1}: {negativeSums[i]}");
+        }
+    }
+}
